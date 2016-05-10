3386f18305eb1650b40d7a9742d251a699b1efb8
Morbus Iff <morbus@disobey.com>
2016-05-10 18:00:17 -0400

    Lots of improvements including documentation, better resume and totals discovery, etc.

--- hathitrust-dl.php
+++ hathitrust-dl.php
@@ -23,87 +23,101 @@
 
 set_time_limit(0);
 
-if (!file_exists('./images')) {
-  if (!mkdir('./images')) {
-    exit("ERROR: Unable to create required ./images directory.\n");
-  }
-}
-
-if (!file_exists('./cbrs')) {
-  if (!mkdir('./cbrs')) {
-    exit("ERROR: Unable to create required ./cbrs directory.\n");
-  }
-}
-
 if (empty($argv[1])) {
-  exit("ERROR: HashiTrust ID is required, ex. php hathitrust-dl.php inu.32000000696932 \"Metropolitan v38n01 (1913-05)\"\n");
-}
-
-$hathitrust_id = $argv[1];
-
-if (!file_exists("./images/$hathitrust_id")) {
-  if (!mkdir("./images/$hathitrust_id")) {
-    exit("ERROR: Unable to create required ./images/$hathitrust_id directory.\n");
-  }
-}
-
-if (!file_exists("./cbrs/$hathitrust_id")) {
-  if (!mkdir("./cbrs/$hathitrust_id")) {
-    exit("ERROR: Unable to create required ./cbrs/$hathitrust_id directory.\n");
-  }
+  exit("ERROR: HathiTrust ID is required, ex. php hathitrust-dl.php inu.32000000696932 \"Metropolitan v38n01 (1913-05)\"\n");
 }
 
 if (empty($argv[2])) {
   exit("ERROR: Desired final name is required, ex. php hathitrust-dl.php inu.32000000696932 \"Metropolitan v38n01 (1913-05)\"\n");
 }
 
-$desired_name = $argv[2];
+// Command line arguments.
+$hathitrust_id  = $argv[1];
+$desired_name   = $argv[2];
 
-$page = 1;
-$batch_not_finished = TRUE;
-while ($batch_not_finished) {
-  $page_number = str_pad($page, 4, 0, STR_PAD_LEFT);
-  $page_filepath = "./images/$hathitrust_id/$desired_name $page_number.jpg";
+// Create directories for storing files.
+foreach (array('images', 'cbrs') as $directory) {
+  if (!file_exists("./$directory")) {
+    if (!mkdir("./$directory/$hathitrust_id", 0777, TRUE)) {
+      exit("ERROR: Unable to create required ./$directory/$hathitrust_id directory.\n");
+    }
+  }
+}
+
+// Load the HathiTrust metadata for the passed ID. None of this data is
+// really important, save for "total_items" which tells us how many pages
+// (images) are in this document. This is useful because HathiTrust appears
+// to return HTTP status code 503 for missing images AND server errors, so
+// just incrementing a page number until we get 404s doesn't work.
+$hathitrust_id_meta_url   = "https://babel.hathitrust.org/cgi/imgsrv/meta?id=$hathitrust_id";
+$hathitrust_id_meta_data  = json_decode(file_get_contents($hathitrust_id_meta_url));
+$total_pages = $hathitrust_id_meta_data->total_items;
+
+print "There are $total_pages pages for HathuTrust ID $hathitrust_id.\n";
+
+// Make an image request for each of the total number of pages.
+for ($current_page = 1; $current_page <= $total_pages; $current_page++) {
+
+  // Save filenames padded as 0001.jpg, 0009.jpg, 0032.jpg, etc., so
+  // that when we rar them up later, they sort properly in CBR readers.
+  $page_number = str_pad($current_page, 4, 0, STR_PAD_LEFT);
+  $page_filepath = "./images/$hathitrust_id/$page_number.jpg";
+
+  if (file_exists($page_filepath)) {
+    print "[$current_page/$total_pages] Skipping already downloaded $page_filepath.\n";
+    continue;
+  }
+
   $fh = fopen($page_filepath, "a");
 
   if (!$fh) {
     exit("ERROR: Unable to create required $page_filepath file.\n");
   }
 
+  // This is just a normal HTTP GET, and there's nothing really special about
+  // it - it's similar to just accessing the below URL in your browser. We
+  // do fake a Chrome user-agent, and tell cURL to save the data to our
+  // $page_filepath filehandle instead of returning the data to a variable.
   $ch = curl_init();
   curl_setopt_array($ch, array(
     CURLOPT_AUTOREFERER     => TRUE,
     CURLOPT_FOLLOWLOCATION  => TRUE,
     CURLOPT_RETURNTRANSFER  => TRUE,
     CURLOPT_FILE            => $fh,
-    CURLOPT_URL             => "https://babel.hathitrust.org/cgi/imgsrv/image?id=$hathitrust_id;seq=$page;width=10000",
+
+    // By default, the "width" argument is the current size of your browser
+    // window when you request a HathiTrust document. If you pass in a very
+    // large size, like 10000 pixels, you'll always receive the largest
+    // image scan available for the current page. Unless it's greater than
+    // 10000 pixels wide, but I suspect they're not scanning at that size yet.
+    CURLOPT_URL             => "https://babel.hathitrust.org/cgi/imgsrv/image?id=$hathitrust_id;seq=$current_page;width=10000",
     CURLOPT_USERAGENT       => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36',
   ));
 
-  if (file_exists($page_filepath)) {
-    $from = filesize($page_filepath);
-    curl_setopt($ch, CURLOPT_RANGE, $from . "-");
-  }
-
-  print "Downloading $page_filepath.\n";
   curl_exec($ch);
 
   if (!curl_errno($ch)) {
     switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
     case 200:
+      print "[$current_page/$total_pages] Downloaded image to $page_filepath.\n";
       break;
+
     default:
+      print "[$current_page/$total_pages] Unexpected HTTP code ($http_code). Retrying.\n";
       unlink($page_filepath);
-      $batch_not_finished = FALSE;
-      exit('Unexpected HTTP code: ' . $http_code . "\n");
+      $current_page--;
+      break;
     }
   }
-
-// @todo Write some error handling.
+  else {
+    exit('ERROR: Exiting due to cURL problem: ' . curl_error($ch) . "\n");
+  }
 
   curl_close($ch);
   fclose($fh);
-  $page++;
+
+  // Good scraper.
+  sleep(rand(0, 6));
 }
 
 system('rar a ' . escapeshellarg("./cbrs/$hathitrust_id/$desired_name.cbr") . ' ' . escapeshellarg("./images/$hathitrust_id/*.jpg"));
